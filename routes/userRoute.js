9e7ab47bb796e5efeb0eaaad61a85b326ab7e8e0
sumit71992 <sumit71992@gmail.com>
2023-03-05 03:56:11 +0530

    test

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/signup', userController.signup);
 router.post('/signin',userController.signin, expenseController.getAllExpenses);
 router.get('/reports', userAuthentication.authenticate, userController.getReport);
-router.get('/download', userAuthentication.authenticate, expenseController.downloadExpense);
+// router.get('/download', userAuthentication.authenticate, expenseController.downloadExpense);
 
 
 module.exports = router;
